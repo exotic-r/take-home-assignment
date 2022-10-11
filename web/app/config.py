900c8b998176045c0c50a0064b7964dbdcff348d
Sho <47435568+exotic-r@users.noreply.github.com>
2022-10-12 02:48:46 +0800

    return fees in chunk

--- web/app/config.py
+++ web/app/config.py
@@ -3,8 +3,19 @@ ETHER_SCAN_API_KEY = 'ZXA5TMY9XTGEY3H953NZEIUYBBK41NYW7W'
 ETHER_SCAN_BASE_URL = 'https://api.etherscan.io/api'
 
 # COIN API
-COIN_API_KEY = '40F8C96C-AD06-4EF2-B337-55989C889C98'
-COIN_API_EXCHANGE_RATE_URL = 'https://rest.coinapi.io/v1/exchangerate/ETH/USDT'
+# COIN_API_KEY = '40F8C96C-AD06-4EF2-B337-55989C889C98'
+# COIN_API_EXCHANGE_RATE_URL = 'https://rest.coinapi.io/v1/exchangerate/ETH/USDT'
 
 UNISWAP_ADDRESS = '0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640'
 
+# ALCHEMY API
+ALCHEMY_API_KEY = 'mtJHDGtUsG0aSjsipsOWwxAbZjxlR2a-'
+ALCHEMY_URL = f'https://eth-mainnet.g.alchemy.com/v2/{ALCHEMY_API_KEY}'
+
+CRYPTO_COMPARE_API_KEY = '316f410cf90a7fd9b4dcc41f8256338516b6675ff04bc829ab3eaae5c8c4e403'
+CRYPTO_COMPARE_URL = 'https://min-api.cryptocompare.com/data/v2/histominute'
+
+# number of transactions to retrieve for each http call to ether scan
+# for the purpose of this exercise, we will keep it small
+ETHER_SCAN_OFFSET = 100
+MAX_FEES_PER_REQUEST = 500
--- web/app/cryptoService.py
+++ web/app/cryptoService.py
@@ -1,13 +1,13 @@
 import json
-from datetime import datetime
+import logging
 from decimal import Decimal
 from http import HTTPStatus
+from typing import Union, Any
 
 import requests
-import logging
-
 from eth_typing import HexStr
 from web3 import Web3
+
 from exceptions import *
 from utils import *
 
@@ -33,7 +33,7 @@ class CryptoService:
         If the fee is not stored in redis, query ether scan to get the fee
         """
         cached_data = self.redis.get(tx_hash)
-        if cached_data:
+        if cached_data is not None:
             return Decimal(cached_data.decode('utf-8').strip('"'))
 
         transaction = self.w3.eth.get_transaction(tx_hash)
@@ -50,11 +50,11 @@ class CryptoService:
         self.redis.set(tx_hash, json.dumps(str(fee)))
         return fee
 
-    def get_transactions_fee_by_time_range(self, start_time: int, end_time: int) -> Decimal:
+    def get_transactions_fee_by_time_range(self, start_time: int, end_time: int) -> list[Union[Decimal, Any]]:
         start_block, end_block = self.get_block_number(start_time), self.get_block_number(end_time)
 
-        def request_fees(page_: int, offset_: int):
-            params = get_ether_scan_params(start_block, end_block, page_, offset_)
+        def request_fees(page_: int):
+            params = get_ether_scan_params(start_block, end_block, page_, ETHER_SCAN_OFFSET)
             response = requests.get(ETHER_SCAN_BASE_URL, params=params)
             if response.status_code != HTTPStatus.OK:
                 raise response.raise_for_status()
@@ -64,41 +64,48 @@ class CryptoService:
                 raise TransactionNotFoundExceptionByTimeRange(
                     start_time, end_time)
 
-            _fees = []
+            chuck = []
             status_code_ = HTTPStatus.OK
 
             for result in response_json['result']:
                 tx_hash = result['hash']
                 cached_data = self.redis.get(tx_hash)
-                if cached_data:
-                    _fees.append(Decimal(cached_data.decode('utf-8').strip('"')))
+                if cached_data is not None:
+                    chuck.append(Decimal(cached_data.decode('utf-8').strip('"')))
                 else:
                     fee, status_code_ = self.calculate_tx_fee(result['gasPrice'], result['gasUsed'],
                                                               result['timeStamp'])
                     if status_code_ in (550, HTTPStatus.TOO_MANY_REQUESTS):
                         break
-                    _fees.append(fee)
+                    chuck.append(fee)
                     self.redis.set(tx_hash, json.dumps(str(fee)))
-            return _fees, status_code_
 
-        # number of transactions to retrieve for each http call to ether scan
-        # for the purpose of this exercise, we will keep it small
-        offset = 10
+            last_processed_time = response_json['result'][-1]['timeStamp']
+            return chuck, status_code_, last_processed_time
+
         page = 1
+        last_processed_time = 0
         fees = []
 
-        while len(fees) % offset == 0:
-            fees, status_code = request_fees(page, offset)
-            fees.extend(fees)
+        while len(fees) % ETHER_SCAN_OFFSET == 0:
+            fees_chuck, status_code, last_processed_time = request_fees(page)
+            fees.extend(fees_chuck)
 
+            if len(fees) >= MAX_FEES_PER_REQUEST:
+                logging.info(f"Processed {len(fees)} transactions, use separate request to get fees")
+                break
             if status_code == HTTPStatus.TOO_MANY_REQUESTS:
                 break
+
             page += 1
-        return fees
+
+        # TODO: handle letting requestor know up to what timstamp have been processed
+
+        return fees, last_processed_time
 
     def get_block_number(self, time_stamp: int) -> int:
         cached_data = self.redis.get(time_stamp)
-        if cached_data:
+        if cached_data is not None:
             return cached_data.decode('utf-8')
 
         params = {
@@ -122,54 +129,51 @@ class CryptoService:
         tx_cost_wei = int(float(gasPrice) * float(gasUsed))
         tx_cost_eth = Web3.fromWei(tx_cost_wei, "ether")
 
-        date_time = CryptoService.__timestamp_to_dateTime(int(time_stamp))
-        rate, status_code = self.__get_fx_rate(date_time)
+        rate, status_code = self.__get_fx_rate(time_stamp)
 
         if not rate:
             return 0, status_code
 
         tx_cost_usdt = rate * tx_cost_eth
 
-        logging.info(f"tx cost - USDT: {tx_cost_usdt:.2f}, wei: {tx_cost_wei:.0f},"
+        logging.info(f"timestamp: {time_stamp}, tx cost - USDT: {tx_cost_usdt:.2f}, wei: {tx_cost_wei:.0f},"
                      f" eth: {tx_cost_eth:.5f}, fx rate: {rate:.2f}")
 
         return tx_cost_usdt, status_code
 
-    def __get_fx_rate(self, date_time) -> tuple[Decimal, int]:
+    def __get_fx_rate(self, timestamp) -> tuple[Decimal, int]:
         """
         Use coin API to get  the fx rate of ETH/USDT at date_time
-        :param date_time:
+        :param timestamp:
         :return:
         """
-        # return Decimal(1300)
         status_code = HTTPStatus.OK
-        cached_data = self.redis.get(date_time)
 
-        if cached_data:
+        cached_data = self.redis.get(timestamp)
+        if cached_data is not None:
             return Decimal(cached_data.decode('utf-8').strip('"')), status_code
 
-        headers = {'X-CoinAPI-Key': COIN_API_KEY}
-        params = {'time': date_time}
-        response = requests.get(
-            COIN_API_EXCHANGE_RATE_URL, params=params, headers=headers)
+        headers = {'Apikey': CRYPTO_COMPARE_API_KEY}
+        params = {'fsym': 'ETH', 'tsym': 'USDC', 'limit': 1, 'toTs': 1665503460}
+        response = requests.get(CRYPTO_COMPARE_URL, params=params, headers=headers)
+
         if response.status_code != HTTPStatus.OK:
             if response.status_code == 550:
                 logging.error(
-                    f"Error retrieving fx rate for date time: {date_time}, returning fx rate 0")
+                    f"Error retrieving fx rate for timestamp: {timestamp}, returning fx rate 0")
                 return Decimal(0), 550
             if response.status_code == HTTPStatus.TOO_MANY_REQUESTS:
-                logging.error(f"Error retrieving fx rate for date time: {date_time} due to too many requests, "
+                logging.error(f"Error retrieving fx rate for date time: {timestamp} due to too many requests, "
                               f"returning fx rate 0")
                 return Decimal(0), HTTPStatus.TOO_MANY_REQUESTS
             raise response.raise_for_status()
 
         response_json = response.json()
+        if response_json.get('Response') != 'Success':
+            logging.error("Failed to retrieve ETH/USDC exchange rate, defaulting to 0")
+            return Decimal(0), 500
 
-        fx_rate = Decimal(response_json['rate'])
-        self.redis.set(date_time, json.dumps(str(fx_rate)))
+        fx_rate = Decimal(response_json['Data']['Data'][0]['open'])
+        self.redis.set(timestamp, json.dumps(str(fx_rate)))
 
         return fx_rate, status_code
-
-    @staticmethod
-    def __timestamp_to_dateTime(timestamp: int) -> str:
-        return datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%dT%H:%M:%SZ')
