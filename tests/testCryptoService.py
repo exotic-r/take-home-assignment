670a3e2e9e7043e28705a69a4fd3ba44168010f9
Sho <47435568+exotic-r@users.noreply.github.com>
2022-10-16 00:22:31 +0800

    make project structure flat

---
+++ tests/testCryptoService.py
@@ -0,0 +1,22 @@
+import unittest
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
+from app.cryptoService import CryptoService
+
+
+class TestCryptoService(TestCase):
+
+    @patch('redis.Redis')
+    def test_get_transaction_fee_by_tx_hash(self, mock_redis):
+        tx_hash = '0x1234'
+        expected = 100.0
+
+        service = CryptoService(mock_redis)
+        result = service.get_transaction_fee_by_tx_hash(tx_hash)
+
+        self.assertEqual(result, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
